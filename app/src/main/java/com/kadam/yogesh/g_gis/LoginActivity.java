7c298a88dfc9bec88977e0777f1f139843c04e07
yogesh2591 <yogesh2591@gmail.com>
2017-04-01 16:43:38 +0530

    Adding Navigation Drawer

--- app/src/main/java/com/kadam/yogesh/g_gis/LoginActivity.java
+++ app/src/main/java/com/kadam/yogesh/g_gis/LoginActivity.java
@@ -1,13 +1,26 @@
 package com.kadam.yogesh.g_gis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gointe = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(gointe);
+            }
+        });
     }
 }
