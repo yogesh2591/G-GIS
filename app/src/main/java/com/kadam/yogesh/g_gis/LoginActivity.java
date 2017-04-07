493435bc13ee240366d9535a6ae683120367e39f
yogesh2591 <yogesh2591@gmail.com>
2017-04-07 21:06:47 +0530

    Adding Cordova library and enhancing Main Activity

--- app/src/main/java/com/kadam/yogesh/g_gis/LoginActivity.java
+++ app/src/main/java/com/kadam/yogesh/g_gis/LoginActivity.java
@@ -5,15 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText editText;
     Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        editText = (EditText) findViewById(R.id.et_username);
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +25,7 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(gointe);
             }
         });
+
+        editText.getText();
     }
 }
