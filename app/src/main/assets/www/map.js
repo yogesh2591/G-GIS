dc6cfb72b65f5a39361de4d9cc976e0b5d4463b1
yogesh2591 <yogesh2591@gmail.com>
2017-04-20 17:12:44 +0530

    Reorgnising project

---
+++ app/src/main/assets/www/map.js
@@ -0,0 +1,31 @@
+var olmap = new ol.Map({
+		  target: 'map',
+		  layers: [
+			new ol.layer.Tile({
+				source: new ol.source.OSM({
+					url: 'http://mt{0-3}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+					attributions: [
+						new ol.Attribution({ html: '� Google' }),
+						new ol.Attribution({ html: '<a href="https://developers.google.com/maps/terms">Terms of Use.</a>' })
+					]
+				})
+			})
+		  ],
+		  view: new ol.View({
+			center: ol.proj.transform(
+				[72.821807,18.974611], 'EPSG:4326', 'EPSG:3857'),
+			zoom: 5
+		  })
+		});
+		
+		var getMapBounds = function(){
+			return olmap.getView().calculateExtent(olmap.getSize());
+		}
+		
+		var zoomtoGPS = function(){
+			var lat = 19.0760;
+			var long = 72.8777;
+			console.log("Long: " + long + " Lat: " + lat);
+			olmap.getView().setCenter(ol.proj.transform([long, lat], 'EPSG:4326', 'EPSG:3857'));
+			olmap.getView.setZoom(23);
+		}
