10fe64c2177defa6964c2c7b925589d30e800109
yogesh2591 <yogesh2591@gmail.com>
2017-04-28 12:29:24 +0530

    Adding Cordova Plugin

---
+++ app/src/main/java/com/kadam/yogesh/g_gis/Plugin/SQLlitePlugin.java
@@ -0,0 +1,10 @@
+package com.kadam.yogesh.g_gis.Plugin;
+
+import org.apache.cordova.CordovaPlugin;
+
+/**
+ * Created by 27957388 on 04/28/17.
+ */
+
+public class SQLlitePlugin extends CordovaPlugin {
+}
