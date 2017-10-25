8d66c984408f97ef53247a69977e6ac6bb0af7f3
yogesh2591@gmail.com <yogesh2591@gmail.com>
2017-10-25 17:27:06 +0530

    jjjjjjjj

--- app/src/main/assets/www/js/map.js
+++ app/src/main/assets/www/js/map.js
@@ -1,82 +1,108 @@
-var hybrid_map_layer = new ol.layer.Tile({
-                       				source: new ol.source.OSM({
-                       					url: 'http://mt{0-3}.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
-                       					attributions: [
-                       						new ol.Attribution({ html: '� Google' }),
-                       						new ol.Attribution({ html: '<a href="https://developers.google.com/maps/terms">Terms of Use.</a>' })
-                       					]
-                       				})
-                       			});
-var standard_map_layer = new ol.layer.Tile({
-                       				source: new ol.source.OSM({
-                       					url: 'http://mt{0-3}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
-                       					attributions: [
-                       						new ol.Attribution({ html: '� Google' }),
-                       						new ol.Attribution({ html: '<a href="https://developers.google.com/maps/terms">Terms of Use.</a>' })
-                       					]
-                       				})
-                       			});
-var open_layer = new ol.layer.Tile({
-            source: new ol.source.OSM()
-          })
-
-var olmap = new ol.Map({
-		  target: 'map',
-		  layers: [
-				open_layer,hybrid_map_layer,standard_map_layer
-		  ],
-		  view: new ol.View({
+Map = (function () {
+	let scaleLineControl = new ol.control.ScaleLine();
+	let hybrid_map_layer = new ol.layer.Tile({
+		source: new ol.source.OSM({
+			url: 'http://mt{0-3}.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
+			attributions: [
+				new ol.Attribution({ html: '� Google' }),
+				new ol.Attribution({ html: '<a href="https://developers.google.com/maps/terms">Terms of Use.</a>' })
+			]
+		})
+	});
+	let standard_map_layer = new ol.layer.Tile({
+		source: new ol.source.OSM({
+			url: 'http://mt{0-3}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+			attributions: [
+				new ol.Attribution({ html: '� Google' }),
+				new ol.Attribution({ html: '<a href="https://developers.google.com/maps/terms">Terms of Use.</a>' })
+			]
+		})
+	});
+	let open_layer = new ol.layer.Tile({
+		source: new ol.source.OSM()
+	})
+	let layers = [open_layer, hybrid_map_layer, standard_map_layer ];
+	let olmap = new ol.Map({
+		controls: ol.control.defaults({
+			attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
+				collapsible: false
+			})
+		}).extend([
+			scaleLineControl
+		]),
+		target: 'map',
+		layers: layers,
+		view: new ol.View({
 			center: ol.proj.transform(
-				[72.821807,18.974611], 'EPSG:4326', 'EPSG:3857'),
+				[72.821807, 18.974611], 'EPSG:4326', 'EPSG:3857'),
 			zoom: 5,
-			minZoom : 4 ,
-			maxZoom : 25
-		  })
-		});
+			minZoom: 4,
+			maxZoom: 25
+		})
+	});
+	
+	return {
+		zoomIn: function () {
+			let zoomLevel = olmap.getView().getZoom() + 1;
+			let view = olmap.getView();
+			view.animate({
+				zoom: zoomLevel,
+				duration : 1000
+			});
+		},
+		zoomOut: function () {
+			let zoomLevel = olmap.getView().getZoom() - 1;
+			let view = olmap.getView();
+			view.animate({
+				zoom: zoomLevel,
+				duration : 1000
+			});
+		},
+		getCurrentZoom: function () {
+			return olmap.getView().getZoom();
+		},
+		getMapExtent: function () {
+			//return olmap.getView().calculateExtent(olmap.getSize());
+			return ol.proj.transform((olmap.getView().calculateExtent(olmap.getSize())), 'EPSG:3857', 'EPSG:4326');
 
-		var getMapBounds = function(){
+		},
+		resetNorth: function () {
+			olmap.getView().setRotation(0);
+		},
+		zoomtoGPS: function (lat, lang) {
+			let zoomLevel = 20;
+			let center = ol.proj.transform([lang, lat], 'EPSG:4326', 'EPSG:3857');
+			let view = olmap.getView();
+			view.animate({
+				center:center,
+				zoom: zoomLevel,
+				duration : 3000
+				
+			});
+		},
+		setBaseMap: function (base_layer_code) {
+			olmap.getLayers().forEach(function (layer, i) {
+				if (layer instanceof ol.layer.Group) {
+					layer.setVisible(true);
+				}					
+				else if (i == base_layer_code) {
+					layer.setVisible(true);
+				} else {
+					layer.setVisible(false);
+				}
 
-			return olmap.getView().calculateExtent(olmap.getSize());
+			});
+
+		},
+		getAllLayersFromMap: function () {
+			return olmap.getLayers();
+		},
+		getScaleLineControl: function () {
+			return scaleLineControl;
+		},
+		getMap: function () {
+			return olmap;
 		}
+	}
 
-		var zoomtoGPS = function(lat,lang){
-          olmap.getView().setCenter(ol.proj.transform([lang, lat], 'EPSG:4326', 'EPSG:3857'));
-           olmap.getView().setZoom(20);
-		}
-
-		var zoomIn = function(){
-
-		    olmap.getView().setZoom(olmap.getView().getZoom() + 1 );
-		}
-
-		var zoomOut = function(){
-
-		olmap.getView().setZoom(olmap.getView().getZoom() -  1 );
-		}
-
-		var resetNorth =function(){
-
-		    olmap.getView().setRotation(0);
-		}
-
-		var setMapLayerVisibility = function (layer_code)
-	    {
-            switch(layer_code)
-            {
-                case 0 :
-                    hybrid_map_layer.setVisible(true);
-                    standard_map_layer.setVisible(false);
-                    open_layer.setVisible(false);
-                break;
-                case 1 :
-                    hybrid_map_layer.setVisible(false);
-                    standard_map_layer.setVisible(true);
-                    open_layer.setVisible(false);
-                break;
-                case 2 :
-                    hybrid_map_layer.setVisible(false);
-                    standard_map_layer.setVisible(false);
-                    open_layer.setVisible(true);
-                break;
-            }
-		 }
+}());
